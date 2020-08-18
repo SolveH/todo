a71d70b2f40a8133f78c66d59e90ca7c138af906
Sølve Bø Hunvik <solveh@netcompany.com>
2020-08-18 13:40:45 +0200

    add function for getting a specific todo

--- src/shared/data.service.js
+++ src/shared/data.service.js
@@ -10,6 +10,19 @@ const getTodos = async function() {
   }
 };
 
+const getTodo = async function(id) {
+  try {
+    const res = await axios.get(`http://localhost:8888/todos/${id}`);
+    let todo = parseItem(res, 200);
+    return todo;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+//Parse response functions
+
 const parseList = (response) => {
   if (response.status !== 200) throw Error(response.message);
   if (!response.data) return [];
@@ -20,6 +33,16 @@ const parseList = (response) => {
   return list;
 };
 
+const parseItem = (response, code) => {
+  if (response.status !== code) throw Error(response.message);
+  let item = response.data;
+  if (typeof item !== "object") {
+    item = undefined;
+  }
+  return item;
+};
+
 export const dataService = {
   getTodos,
+  getTodo,
 };
