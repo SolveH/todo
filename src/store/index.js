6f22530da272261a5be414147a263c29f853c2cd
maalfrid <malfridha@gmail.com>
2020-08-18 14:04:42 +0200

    Initial setup of store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,33 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    todos: []
+  },
+  getters: {
+    getTodos: state => state.todos
+  },
+  mutations: {
+    addTodo: (state, id, text) => {
+      const newTodo = {
+        id: id,
+        name: text,
+        complete: false
+      };
+      state.todos.push(newTodo);
+    },
+    deleteTodo: (state, todo) => {
+      var index = state.todos.indexOf(todo);
+      state.todos.splice(index, 1);
+    }
+  },
+  actions: {
+    addTodo: (context, id, text) => {
+      context.commit("addTodo", id, text);
+    },
+    deleteTodo: (context, todo) => {
+      context.commit("deleteTodo", todo);
+    }
+  },
   modules: {}
 });
