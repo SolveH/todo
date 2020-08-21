297d20ac0ef98e8064f841a3a21a220fa42a91d3
maalfrid <malfridha@gmail.com>
2020-08-21 11:21:19 +0200

    Initial tests

--- api/src/test/java/org/brogrammers/todoapi/TodoControllerTest.java
+++ api/src/test/java/org/brogrammers/todoapi/TodoControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,6 +28,17 @@ public class TodoControllerTest {
     void whenGetTodos_thenReturnHTTPStatusOK() throws Exception {
         mvc.perform(get("/todo"))
                 .andExpect(status().isOk());
+        mvc.perform(get("/todo/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenCreateTodo_thenReturnTodoAndHTTPStatusCreated() throws Exception {
+        // make
+        Todo todo = new Todo("gjfds", "This is a todo", false);
+        // put
+        mvc.perform(put("/todo/", todo))
+                .andExpect(status().isCreated());
     }
 
 }
