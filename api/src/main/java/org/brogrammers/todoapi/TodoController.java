8745a887b78d30239c9f028c4012130c90639326
Sølve Bø Hunvik <solveh@netcompany.com>
2020-08-25 14:37:34 +0200

    remove extra autowired

--- api/src/main/java/org/brogrammers/todoapi/TodoController.java
+++ api/src/main/java/org/brogrammers/todoapi/TodoController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TodoController {
 
-    @Autowired
+
     private TodoRepository todoRepository;
 
     @Autowired
