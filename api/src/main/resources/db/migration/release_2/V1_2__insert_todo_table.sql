f5e66c468e175af4aee9cad675294c0f8e1c7432
Sølve Bø Hunvik <solveh@netcompany.com>
2020-08-26 13:45:33 +0200

    add more to the insert migration

--- api/src/main/resources/db/migration/release_2/V1_2__insert_todo_table.sql
+++ api/src/main/resources/db/migration/release_2/V1_2__insert_todo_table.sql
@@ -1,5 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 INSERT INTO todo ( id, name, complete)
 VALUES
-       (uuid_generate_v4(), 'Do your stuff', DEFAULT),
-       (uuid_generate_v4(), 'Wash you', DEFAULT);
+       (uuid_generate_v4(), 'Wash the dishes', DEFAULT),
+       (uuid_generate_v4(), 'Feed your squirrel', DEFAULT),
+       (uuid_generate_v4(), 'Drink Coca-Cola', DEFAULT),
+       (uuid_generate_v4(), 'Work out', DEFAULT),
+       (uuid_generate_v4(), 'Pet the dog', DEFAULT);
